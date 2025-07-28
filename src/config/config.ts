d7fcf247cb1d1288fb8756ef1b6f6adddd49787c
Abhishek_05 <108204294+ap20000@users.noreply.github.com>
2025-07-28 11:46:14 +0545

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,5 +2,5 @@
 // // export const API_BASE_URL = "http://localhost:5000/api";
 
 
-export const SOCKET_BASE_URL = "https://hamrogamingstore.com";
-export const API_BASE_URL = "https://hamrogamingstore.com";
+export const SOCKET_BASE_URL = "https://hamrogamingstore.com/backend";
+export const API_BASE_URL = "https://hamrogamingstore.com/backend";
