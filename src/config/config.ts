33bda8956b7752fa5d1ef213b2657c24b9e5fcbe
Abhishek_05 <108204294+ap20000@users.noreply.github.com>
2025-08-05 10:27:21 +0545

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,3 +4,10 @@
 
 export const API_BASE_URL = "https://hamrogamingstore.com/backend";
 export const SOCKET_BASE_URL = "https://hamrogamingstore.com";
+
+// export const API_BASE_URL = "https://hamro-gaming.onrender.com";
+// // export const API_BASE_URL = "http://localhost:5000/api";
+
+
+// export const SOCKET_BASE_URL = "https://hamro-gaming.onrender.com";
+// export const API_BASE_URL = "https://hamro-gaming.onrender.com";
